93b97b7a20ee05130d4e898e0df28a5ec4467614
nagarekawa <nagarekawa1011@gmail.com>
2021-10-21 09:23:39 +0900

    ohisa

--- abc/abc211/c.py
+++ abc/abc211/c.py
@@ -1,16 +1,15 @@
 
 s=input()
-le=len(s)
+
 namae='chokudai'
-dict_2={s:0 for s in namae}
-dict_1={s:-1 for s in namae}
-for i in range(len(s)):
-    if(dict_2[s[-i-1]]==0):
-        dict_2[s[-i-1]]=le-i-1
-print(dict_2)
-for i in range(len(s)):
-    if(dict_1[s[i]]==-1):
-        dict_1[s[i]]=i
-print(dict_1)
-print(len(dict_1))
-# for i in range(len(dict)):
+
+dp=[[0 for j in range(8+1)] for i in range(len(s)+1)]
+for i in range(len(s)+1):
+    dp[i][0]=1
+for i in range(1,len(s)+1):
+    for j in range(1,8+1):
+        if(s[i-1]==namae[j-1]):
+            dp[i][j]=(dp[i-1][j-1]+dp[i-1][j])%(10**9+7)
+        else:
+            dp[i][j]=dp[i-1][j]
+print(dp[-1][-1]%(10**9+7))
--- abc/abc211/d.py
+++ abc/abc211/d.py
@@ -9,23 +9,20 @@ for i in range(m):
     lis[b[i]-1].append(a[i]-1)
 seen=[0]*n
 ans=[0]*n
-def dfs(start,hukasa):
-    seen[start]=True
-    for i in lis[start]:
-        if(seen[i]==False):
-            seen[i]=True
-            print(i)
-            if(i==n-1):
-                ans[hukasa]=ans[hukasa]+1
-            else:
-                dfs(i,hukasa+1)
-hukasa=0
-print(lis)
-dfs(0,hukasa)
-print(ans)
-print('----------')
-for i in range(n):
-    if(ans[i]!=0):
-        print(ans[i])
-        exit()
-print(0)
+
+
+que=[0] #初期値は最初の探索の開始地点  forを回すことでqueに探索したところが追加される
+dist=[None]*n #最初の探索の開始地点からの距離
+cnt=[0]*n #添字の距離でどのくらいパターンが有るか
+dist[0]=0#最初のところに行くためには移動しなくて良いため０
+cnt[0]=1#最初のところに行くパターンは一個
+
+for i in que:
+    for j in lis[i]:
+        if dist[j] is None:
+            dist[j]=dist[i]+1
+            que.append(j)
+            cnt[j]=cnt[i]
+        elif dist[j]==dist[i]+1:
+            cnt[j]=(cnt[i]+cnt[j])%(10**9+7)
+print(cnt[-1])
