5a5cfa3eeead3fca3a442518bdd995c93634eeca
nagarekawa <nagarekawa1011@gmail.com>
2022-02-26 18:30:12 +0900

    'abc214_c_win'

--- abc/abc214/c.py
+++ abc/abc214/c.py
@@ -3,33 +3,21 @@ n=int(input())
 s=list(map(int,input().split()))
 t=list(map(int,input().split()))
 
-#
-# ans=[float('inf') for i in range(n)]
-# mae=float('inf')
-#
-# ans[0]=t[0]
-# mae=ans[0]+s[0]
-# for i in range(1,2*n+1):
-#     if(mae<t[i%n]):
-#         ans[i%n]=min(ans[i%n],mae)
-#         mae=ans[i%n]+s[i%n]
-#
-#
-#     else:
-#         ans[i%n]=min(ans[i%n],t[i%n])
-#         mae=ans[i%n]+s[i%n]
-#
-# for i in range(n):
-#     print(ans[i])
 
-import heapq
-que=[]
-heapq.heapify(que)
+ans=[float('inf') for i in range(n)]
+mae=float('inf')
+
+ans[0]=t[0]
+mae=ans[0]+s[0]
+for i in range(1,2*n+1):
+    if(mae<t[i%n]):
+        ans[i%n]=min(ans[i%n],mae)
+        mae=ans[i%n]+s[i%n]
+
+
+    else:
+        ans[i%n]=min(ans[i%n],t[i%n])
+        mae=ans[i%n]+s[i%n]
+
 for i in range(n):
-    que.append([t[i],i])
-print(que)
-print(type(que))
-heapq.heapify(que)
-print(type(que))
-print(que)
-print(heapq.heappop(que))
+    print(ans[i])
